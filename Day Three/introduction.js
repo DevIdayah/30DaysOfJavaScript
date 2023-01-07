6b8d47f2d7b24011629f098f3c031ed5cc426365
DevIdayah <97677077+DevIdayah@users.noreply.github.com>
2023-01-07 21:12:54 +0100

    features:i learnt boolean and operator

--- Day Three/introduction.js
+++ Day Three/introduction.js
@@ -3,3 +3,30 @@ console.log(firstName)
 
 let empty = null
 console.log(empty)
+
+let numOne = 4
+let numTwo = 3
+let sum = numOne + numTwo
+let diff = numOne - numTwo
+let mult = numOne * numTwo
+let div = numOne / numTwo
+let remainder = numOne % numTwo
+let powerOf = numOne ** numTwo
+console.log(sum, diff, mult, div, remainder, powerOf)
+
+let radius = 3
+const PI = 3.14
+let answer = radius * PI
+console.log(answer)
+
+const check = 4 > 3 || 10 > 5
+const checck = 4 > 3 || 10 < 5
+const checkk = 4 < 3 || 10 < 5
+console.log(check, checck, checkk)
+
+//increment operator
+let count = 3
+console.log(++count)
+console.log(count)
+console.log(count++)
+console.log(count)
