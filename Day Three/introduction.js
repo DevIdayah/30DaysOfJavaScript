d3e3f4729d90c10c1ce164a5ec3c9749e45c5247
DevIdayah <97677077+DevIdayah@users.noreply.github.com>
2023-01-08 22:02:47 +0100

    i learnt Ternary operator and Windows methods

--- Day Three/introduction.js
+++ Day Three/introduction.js
@@ -30,3 +30,25 @@ console.log(++count)
 console.log(count)
 console.log(count++)
 console.log(count)
+
+//Ternary operator
+let isRaining = true
+isRaining
+    ? console.log('You need a rain coat.')
+    : console.log('No need for a rain coat.')
+isRaining = false
+
+isRaining
+    ? console.log('You need a rain coat.')
+    : console.log('No need for a rain coat.')
+
+//Windows alert method
+alert('Welcome to 30DaysOfJavaScript')
+
+//Windows prompt method
+let number = prompt('Enter number', 'number goes here')
+console.log(number)
+
+//Windows confirm method
+const agree = confirm('Are you sure you like to delete? ')
+console.log(agree)
