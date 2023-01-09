5c383fe1359f1bb29afaef2a2cab40daa0366cec
DevIdayah <97677077+DevIdayah@users.noreply.github.com>
2023-01-09 15:50:01 +0100

    features:if statement,if else statement and if else if else statement

--- Day Four/conditions.js
+++ Day Four/conditions.js
@@ -1,4 +1,46 @@
-let rain = false
+//IF STATEMENT
+let rain = true
 if (rain) {
-    console.log('you don\'t need a rain coat')
+    console.log(`you need a rain coat`)
+}
+else {
+    console.log(`you don\'t need a rain coat`)
+}
+
+//IF ELSE STATEMENT
+let myAge = 25
+if (myAge <= 23) {
+    console.log(`you are still a small girl`)
+} else {
+    console.log(`you are an adult`)
+}
+
+let isMarried = true
+if (isMarried == false) {
+    console.log(`she is still single and searching`)
+} else {
+    console.log(`congratulations darling`)
+}
+
+//IF ELSE IF ELSE STATEMENT
+let a = 0
+if (a > 0) {
+    console.log(`${a} is a positive number`)
+} else if (a < 0) {
+    console.log(`${a} is a negative number`)
+} else if (a == 0) {
+    console.log(`${a} is zero`)
+} else {
+    console.log(`${a} is not a number`)
+}
+
+let age = 23
+if (age > 23) {
+    console.log(`age is greater than ${age}`)
+} else if (age < 23) {
+    console.log(`age is less than ${age}`)
+} else if (age <= 23) {
+    console.log(`${age} is my age`)
+} else {
+    console.log(`she is underage`)
 }
