322f30a4883d64696ee81e7a58b5483e275a0da7
DevIdayah <97677077+DevIdayah@users.noreply.github.com>
2023-01-11 09:21:25 +0100

    Day five:i learnt how to create an array with vaules,how to create an empty array and how to create an array using split

---
+++ Day Five/array.js
@@ -0,0 +1,50 @@
+const numbers = [0, 3.14, 9.81, 37, 98.6, 100] // array of numbers
+const fruits = ['banana', 'orange', 'mango', 'lemon'] // array of strings, fruits
+const vegetables = ['Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot'] // array of strings, vegetables
+const animalProducts = ['milk', 'meat', 'butter', 'yoghurt'] // array of strings, products
+const webTechs = ['HTML', 'CSS', 'JS', 'React', 'Redux', 'Node', 'MongDB'] // array of web technologies
+const countries = ['Finland', 'Denmark', 'Sweden', 'Norway', 'Iceland'] // array of strings, countries
+
+// Print the array and its length
+
+console.log('Numbers:', numbers)
+console.log('Number of numbers:', numbers.length)
+
+console.log('Fruits:', fruits)
+console.log('Number of fruits:', fruits.length)
+
+console.log('Vegetables:', vegetables)
+console.log('Number of vegetables:', vegetables.length)
+
+console.log('Animal products:', animalProducts)
+console.log('Number of animal products:', animalProducts.length)
+
+console.log('Web technologies:', webTechs)
+console.log('Number of web technologies:', webTechs.length)
+
+console.log('Countries:', countries)
+console.log('Number of countries:', countries.length)
+
+// arr containing different data types        
+const arr = [
+    'Asabeneh',
+    250,
+    true,
+    { country: 'Finland', city: 'Helsinki' },
+    { skills: ['HTML', 'CSS', 'JS', 'React', 'Python'] }
+]
+console.log(arr)
+
+let info = ['idayat', 'Zakariyah', 23, 'single',
+    { Country: 'Nigeria', State: 'Kwara' },
+    { skills: ['HTML', 'CSS', 'TAILWINDCSS', 'JAVASCRIPT', 'REACT'] }
+]
+console.log(info)
+
+let career = 'frontend'
+let myCareer = career.split('')
+console.log(myCareer)
+
+let skills = 'HTML, CSS ,TAILWINDCSS, JAVASCRIPT, REACT'
+const mySkills = skills.split(',')
+console.log(mySkills)
