30ebe1ba346457d62438e35b36db6b27932016d8
DevIdayah <97677077+DevIdayah@users.noreply.github.com>
2023-01-15 23:05:12 +0100

    array continuation

--- Day Five/array.js
+++ Day Five/array.js
@@ -48,3 +48,21 @@ console.log(myCareer)
 let skills = 'HTML, CSS ,TAILWINDCSS, JAVASCRIPT, REACT'
 const mySkills = skills.split(',')
 console.log(mySkills)
+
+const webTechs = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React',
+  'Redux',
+  'Node',
+  'MongoDB'
+] // List of web technologies
+
+console.log(webTechs)        // all the array items
+console.log(webTechs.length) // => to know the size of the array, which is 7
+console.log(webTechs[0])     //  -> HTML
+console.log(webTechs[6])     //  -> MongoDB
+
+let lastIndex = webTechs.length - 1
+console.log(webTechs[lastIndex])
